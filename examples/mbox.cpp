cad543aa6cbdeaa899aed6c7d47e14b17c5fe22a
mint-tube <mmooyrs@gmail.com>
2026-06-16 22:32:39 +0300

    mbox: make Style a namespace

--- examples/mbox.cpp
+++ examples/mbox.cpp
@@ -1,53 +1,48 @@
 #include "../include/mbox.hpp"
 
+using namespace mbox;
+using namespace mbox::style;
+
 int main() {
   {
-    mbox::term term;
+    term term;
     size_t y = 0;
 
-    term.printf(0, y++, Style::NONE, Style::NONE,
-      "привет from mbox!");                                          // full support of UTF-8
-    term.printf(0, y++, Style::NONE, Style::NONE,
-      "width=%d height=%d", term.get_width(), term.get_height());    // string formatting
-    term.printf(0, y++, Style::BLUE | Style::BRIGHT, Style::NONE,    // text attributes
-      "PRESS <ESC> to quit");
-    term.printf(0, y++, Style::BLUE | Style::BRIGHT, Style::NONE,
-      "PRESS <TAB> to toggle mouse capture");
+    term.printf(0, y++, 0, 0, "привет from mbox!");                  // full support of UTF-8
+    term.printf(0, y++, 0, 0, "width=%d height=%d", term.get_width(), term.get_height());
+    term.printf(0, y++, BLUE | BRIGHT, 0, "PRESS <ESC> to quit");    // text attributes from mbox::style
+    term.printf(0, y++, BLUE | BRIGHT, 0, "PRESS <TAB> to toggle mouse capture");
     term.flush();                                                    // flush the buffer manually
     y++;
 
     bool mouse_mode = true;
     term.capture_mouse(mouse_mode);                                  // opt-in for mouse input
 
-    mbox::event ev;
+    event ev;
     while (true) {
       if (y >= term.get_height()) {
         term.clear();
         y = 0;
-      };
+      }
 
       ev = term.peek_event(30 * 1000);                               // poll for 30 seconds
       if (ev.key == Key::ESC) break;
       if (ev.key == Key::TAB) term.capture_mouse(mouse_mode = !mouse_mode);
 
       switch (ev.type) {
-        case EventType::KEY:                                         // process every sent key individually
+        case EventType::KEY:                                         // process every keystroke
           char utf8[7];
           mbox::utf32_to_utf8(utf8, ev.ch);
-          term.printf(0, y++, Style::YELLOW, Style::NONE,
-            "KEY:    mod=%d key=%d char=%s", ev.mod, ev.key, utf8);
+          term.printf(0, y++, YELLOW, 0, "KEY:    mod=%d key=%d char=%s", ev.mod, ev.key, utf8);
           break;
         case EventType::RESIZE:                                      // react to resizing
-          term.printf(0, y++, Style::CYAN | Style::BRIGHT, Style::NONE,
-            "RESIZE: width=%d height=%d", ev.width, ev.height);
+          term.printf(0, y++, CYAN | BRIGHT, 0, "RESIZE: width=%d height=%d", ev.width, ev.height);
           break;
-        case EventType::MOUSE:                                       // capture mouse clicking and scrolling
-          term.printf(0, y++, Style::GREEN, Style::NONE,
-            "MOUSE:  button=%d x=%d y=%d", ev.button, ev.x, ev.y);
+        case EventType::MOUSE:                                       // capture mouse interactions
+          term.printf(0, y++, GREEN, 0, "MOUSE:  button=%d x=%d y=%d", ev.button, ev.x, ev.y);
           break;
         case EventType::NONE:
-          term.printf(0, y++, Style::MAGENTA, Style::NONE,
-            "NONE:   nothing happen for 30 seconds");
+          term.printf(0, y++, MAGENTA, 0, "NONE:   nothing happen for 30 seconds");
       }
 
       term.flush();
