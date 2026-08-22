744945f58e5ee6f2c1c8ce58fe3da29e49fad848
mint-tube <mmooyrs@gmail.com>
2026-08-22 10:49:37 +0300

    mbox: fix licensing

--- include/mbox.hpp
+++ include/mbox.hpp
@@ -2385,9 +2385,27 @@ namespace mbox {
 }
 
 /*
-  Copyright (c) 2026 Vsevolod Khoroshavtcev <mmooyrs@gmail.com>
+  MIT License
 
-  Redistribution and use of this file, with or without modification,
-  are permitted in any medium without royalty provided this notice is preserved.
-  This file is offered as-is, without any form of warranty.
+  Copyright (c) 2026      Vsevolod Khoroshavtcev <mmooyrs@gmail.com>
+                2015-2026 Adam Saponara <as@php.net>
+                2010-2020 nsf <no.smile.face@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
 */
